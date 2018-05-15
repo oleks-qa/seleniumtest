d802f82134cb42691678bf68774b8b6d932bc171
Alex <sasha044@gmail.com>
2018-05-15 13:06:08 +0300

    Adding basic SeleniumTest

---
+++ SeleniumTest/src/com/automation/selenium/Main.java
@@ -0,0 +1,16 @@
+package com.automation.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("https://google.com");
+        webDriver.findElement(By.id("lst-ib")).sendKeys("selenium");
+        webDriver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+    }
+}
